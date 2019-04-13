ae66b8d74d4473339d0dcb4962fcd6107688c676
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-04-13 15:50:39 +0700

    - Build layout for builder-field

--- src/app/views/pages/builder/builder.component.ts
+++ src/app/views/pages/builder/builder.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { ModalDialogComponent } from './../material/controls/modal-dialog/modal-dialog.component';
+import { ModalBottomSheetComponent } from "./../material/controls/modal-bottom-sheet/modal-bottom-sheet.component";
+import { Component, OnInit } from "@angular/core";
+import { MatBottomSheet, MatDialog } from "@angular/material";
 
 @Component({
-  selector: 'kt-builder',
-  templateUrl: './builder.component.html',
-  styleUrls: ['./builder.component.scss']
+	selector: "kt-builder",
+	templateUrl: "./builder.component.html",
+	styleUrls: ["./builder.component.scss"]
 })
 export class BuilderComponent implements OnInit {
+	panelOpenState: boolean = false;
+	data = {
+		animal: "panda"
+	};
+	constructor(
+		// private bottomSheet: MatBottomSheet,
+		// public dialog: MatDialog
+	) {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+	// openBottomSheet(): void {
+	// 	this.bottomSheet.open(ModalBottomSheetComponent, {
+	// 		data: { names: ["Frodo", "Bilbo"], panelClass: "container" }
+	// 	});
+	// }
+	// openDialog() {
+	// 	this.dialog.open(ModalDialogComponent, {
+	// 		data: this.data,
+	// 		width: '80%',
+	// 		panelClass: ''
+	// 	});
+	// }
 }
