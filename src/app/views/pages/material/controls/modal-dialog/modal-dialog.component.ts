d53349e00a7c3d67c692a965f53fc3be3aec159f
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-04-13 21:57:59 +0700

    - Modify layout in dialog

--- src/app/views/pages/material/controls/modal-dialog/modal-dialog.component.ts
+++ src/app/views/pages/material/controls/modal-dialog/modal-dialog.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { Component, OnInit, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'kt-modal-dialog',
-  templateUrl: './modal-dialog.component.html',
-  styleUrls: ['./modal-dialog.component.scss']
+	selector: "kt-modal-dialog",
+	templateUrl: "./modal-dialog.component.html",
+	styleUrls: ["./modal-dialog.component.scss"]
 })
 export class ModalDialogComponent implements OnInit {
-  constructor(
-		@Inject(MAT_DIALOG_DATA) public data: any
-	) {
+	constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+		// this.bindData();
 		console.log(data);
 	}
+	toppings = new FormControl();
+	toppingList = [
+		"Extra cheese",
+		"Mushroom",
+		"Onion",
+		"Pepperoni",
+		"Sausage",
+		"Tomato"
+	];
 
 	openLink(event: MouseEvent): void {
 		// this.bottomSheetRef.dismiss();
@@ -19,4 +28,10 @@ export class ModalDialogComponent implements OnInit {
 	}
 
 	ngOnInit() {}
+	// default value multiple select
+	bindData() {
+		const anotherList: any[] = [this.toppingList[0], this.toppingList[1]];
+
+		this.toppings.setValue(anotherList);
+	}
 }
