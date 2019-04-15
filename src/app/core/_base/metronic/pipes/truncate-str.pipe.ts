b38691b47a110a6afd4be7ce2d8d8884fa745979
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-04-15 14:54:11 +0700

    - Update check value null, undefined, empty

--- src/app/core/_base/metronic/pipes/truncate-str.pipe.ts
+++ src/app/core/_base/metronic/pipes/truncate-str.pipe.ts
@@ -1,26 +1,33 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'truncate'
+	name: "truncate"
 })
 export class TruncateStrPipe implements PipeTransform {
+	transform(
+		value: string,
+		limit = 25,
+		completeWords = false,
+		ellipsis = "..."
+	) {
+		if (!value) {
+			return "";
+		}
+		if (completeWords) {
+			limit = value.substr(0, limit).lastIndexOf(" ");
+		}
+		return `${value.substr(0, limit)}${ellipsis}`;
+	}
+	// public longStr = 'A really long string that needs to be truncated';
+	// <h1>{{longStr | truncate }}</h1>
+	// <!-- Outputs: A really long string that... -->
 
-  transform(value: string, limit = 25, completeWords = false, ellipsis = '...') {
-    if (completeWords) {
-      limit = value.substr(0, limit).lastIndexOf(' ');
-    }
-    return `${value.substr(0, limit)}${ellipsis}`;
-  }
-  // public longStr = 'A really long string that needs to be truncated';
-  // <h1>{{longStr | truncate }}</h1> 
-  // <!-- Outputs: A really long string that... -->
+	// <h1>{{longStr | truncate : 12 }}</h1>
+	// <!-- Outputs: A really lon... -->
 
-  // <h1>{{longStr | truncate : 12 }}</h1> 
-  // <!-- Outputs: A really lon... -->
+	// <h1>{{longStr | truncate : 12 : true }}</h1>
+	// <!-- Outputs: A really... -->
 
-  // <h1>{{longStr | truncate : 12 : true }}</h1> 
-  // <!-- Outputs: A really... -->
-
-  // <h1>{{longStr | truncate : 12 : false : '***' }}</h1> 
-  // <!-- Outputs: A really lon*** -->
+	// <h1>{{longStr | truncate : 12 : false : '***' }}</h1>
+	// <!-- Outputs: A really lon*** -->
 }
