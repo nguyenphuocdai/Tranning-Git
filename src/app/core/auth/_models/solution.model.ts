6c7caa67b81aeacadc38a4c0f4a5880f2cbbfab6
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-04-17 23:32:17 +0700

    - Change name class Solution --> SolutionModel

--- src/app/core/auth/_models/solution.model.ts
+++ src/app/core/auth/_models/solution.model.ts
@@ -1,6 +1,10 @@
 import { BaseBuilderModel } from './../../_base/crud/models/_base-builder.model';
 
-export class Solution extends BaseBuilderModel {
+export class SolutionModel extends BaseBuilderModel {
     roles: number[];
     image: string;
+
+    clear(): void {
+       // clear solution
+    }
 }
