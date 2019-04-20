69ea5bd18ae330ac08c7b58da0e93e69c2ce8648
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-04-20 20:20:45 +0700

    - Add functionbase ultilities

---
+++ src/app/core/_function-base/function-base.ultilities.ts
@@ -0,0 +1,11 @@
+/**
+ * Process format logic for module in app
+ */
+export default class FunctionBase {
+	static doSomething(val: string) {
+		return val;
+	}
+	static doSomethingElse(val: string) {
+		return val;
+	}
+}
