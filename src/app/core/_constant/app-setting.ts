7c626712a4ad3adf15a1611bc3ad9957cee84184
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-04-20 20:21:12 +0700

    - Fix syntax, add simulator API for module,form field

--- src/app/core/_constant/app-setting.ts
+++ src/app/core/_constant/app-setting.ts
@@ -3,4 +3,5 @@ export class AppSettings {
 
 	public static API_SOLUTIONS_URL = "http://127.0.0.1:9999/api/";
 	public static API_MODULE_URL = "http://127.0.0.1:9999/api/";
+	public static API_FORM_FIELD_URL = "http://127.0.0.1:9999/api/";
 }
