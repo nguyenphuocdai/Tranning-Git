e9ceff0423f73c294ba855787fdc1b09381c644b
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-04-26 01:53:21 +0700

    - Add directive users only input numbers or decimal

---
+++ src/app/core/_base/metronic/directives/only-input-digits.ts
@@ -0,0 +1,52 @@
+import { Directive, ElementRef, HostListener, Input } from "@angular/core";
+
+@Directive({
+	selector: "[ktNumbericDerective]"
+})
+export class NumericDirective {
+	/**
+	 *    Allow decimal numbers and negative values
+	 */
+	// private regex: RegExp = new RegExp(/^-?[0-9]+(\.[0-9]*){0,1}$/g);
+	/**
+	 *    Allow "/(09|01[2|6|8|9])+([0-9]{8})\b/g" ---- phone number
+	 */
+	@Input("numericType") numericType: string; // number | decimal
+
+	/**
+	 *    Allow numbers and decimal
+	 */
+
+	private regex = {
+		number: new RegExp(/^\d+$/),
+		decimal: new RegExp(/^[0-9]+(\.[0-9]*){0,1}$/g)
+	};
+	/**
+	 * Not allow specialKeys input
+	 */
+	private specialKeys = {
+		number: ["Backspace", "Tab", "End", "Home", "ArrowLeft", "ArrowRight"],
+		decimal: ["Backspace", "Tab", "End", "Home", "ArrowLeft", "ArrowRight"]
+	};
+
+	constructor(private el: ElementRef) {}
+
+	/**
+	 * keydown check validator input
+	 * @param event
+	 */
+	@HostListener("keydown", ["$event"])
+	onKeyDown(event: KeyboardEvent) {
+		if (this.specialKeys[this.numericType].indexOf(event.key) !== -1) {
+			return;
+		}
+		// Do not use event.keycode this is deprecated.
+		let current: string = this.el.nativeElement.value;
+		let next: string = current.concat(event.key);
+		if (next && !String(next).match(this.regex[this.numericType])) {
+			event.preventDefault();
+		}
+	}
+
+	// example <input ktNumberOnlyDerective numericType="decimal" type="text">
+}
