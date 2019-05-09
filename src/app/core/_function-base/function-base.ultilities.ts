e08257556bd415cbe6de200152ed4ba482127d7b
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-10 02:23:47 +0700

    - Change functionbase structor

--- src/app/core/_function-base/function-base.ultilities.ts
+++ src/app/core/_function-base/function-base.ultilities.ts
@@ -1,14 +1,21 @@
 /**
  * Process format logic for module in app
  */
-export default class FunctionBase {
-	static replaceSpace(val: string, separate: string = "_") {
-		if (val) {
-			return;
-		}
-		return `${val.replace(/ /g, separate)}`;
-	}
-	static doSomethingElse(val: string) {
-		return val;
-	}
+// export default class FunctionBase {
+// 	static replaceSpace(val: string, separate: string = "_") {
+// 		if (val) {
+// 			return;
+// 		}
+// 		return `${val.replace(/ /g, separate)}`;
+// 	}
+// 	static doSomethingElse(val: string) {
+// 		return val;
+// 	}
+// 	static isEmptyOrSpaces(str: string) {
+// 		return str === null || str.match(/^ *$/) !== null;
+// 	}
+// }
+
+export function isEmptyOrSpaces(str: string) {
+	return str === null || str.match(/^ *$/) !== null;
 }
