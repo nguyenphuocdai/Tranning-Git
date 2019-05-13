49fe44f06950f2535e52da93e68587190c071737
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-14 00:53:14 +0700

    - Add constant for form-controls

--- src/app/core/_constant/field-setting.ts
+++ src/app/core/_constant/field-setting.ts
@@ -1,4 +1,8 @@
 export class FieldSetting {
 	public static INPUT = "input";
 	public static AUTOCOMPLETE = "autocomplete";
+	public static CHECKBOX = "checkbox";
+	public static DATEPICKER = "datepicker";
+	public static RADIOBUTTON = "radiobutton";
+	public static SELECTOPTION = "selectoption";
 }
