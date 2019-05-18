50d40c936d453ef3763da27760aec32b213ca241
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-18 19:33:31 +0700

    - Add toggle comma with input for number

---
+++ src/app/core/_base/metronic/directives/toggle-comma-input.ts
@@ -0,0 +1,27 @@
+import { Directive, Renderer, HostListener, ElementRef, Output, EventEmitter } from "@angular/core";
+
+@Directive({
+	selector: "[ktInputToggleComma]"
+})
+export class InputToggleCommaDirective {
+	constructor(private el: ElementRef, private renderer: Renderer) {}
+    @Output() ngModelChange: EventEmitter<any> = new EventEmitter();
+	@HostListener("focus") onFocus() {
+		if (this.el.nativeElement.value.length <= 3) {
+			return;
+		}
+		let valueFocus = this.el.nativeElement.value.replace(",", "");
+        this.ngModelChange.emit(valueFocus);
+	}
+
+	@HostListener("blur") onBlur() {
+		if (this.el.nativeElement.value.length <= 3) {
+			return;
+		}
+		let valueBlur = this.el.nativeElement.value.replace(
+			/\B(?=(\d{3})+(?!\d))/g,
+			","
+		);
+		this.ngModelChange.emit(valueBlur);
+	}
+}
