a94ef1f6eb40bbc0b6161a85b1f75c3926954dc7
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-19 16:12:52 +0700

    - Move number component to folder number

--- src/app/views/pages/builder-manage/form-field-management/components/number/number.component.ts
+++ src/app/views/pages/builder-manage/form-field-management/components/number/number.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup } from "@angular/forms";
-import { FieldConfigInterface } from "../../../../../core/auth";
+import { FieldConfigInterface } from "../../../../../../core/auth";
 @Component({
 	selector: "app-number",
 	template: `
