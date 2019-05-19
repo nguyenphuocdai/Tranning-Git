ba49364d445f50f5b0c1f7015b5bbc356c4922eb
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-19 16:13:07 +0700

    - Move functionbase to types ultis services

--- src/app/core/_base/crud/utils/types-utils.service.ts
+++ src/app/core/_base/crud/utils/types-utils.service.ts
@@ -1,5 +1,5 @@
 /** Angular */
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class TypesUtilsService {
@@ -12,7 +12,7 @@ export class TypesUtilsService {
 		if (this.isNumber(value)) {
 			return `0${value}`.slice(-2);
 		} else {
-			return '';
+			return "";
 		}
 	}
 
@@ -47,7 +47,7 @@ export class TypesUtilsService {
 			return `${month}/${day}/${year}`;
 		}
 
-		return '';
+		return "";
 	}
 
 	/**
@@ -56,14 +56,18 @@ export class TypesUtilsService {
 	 * @param date: any
 	 */
 	dateCustomFormat(date: any): string {
-		let stringDate: string = '';
+		let stringDate: string = "";
 		if (date) {
-			stringDate += this.isNumber(date.month) ? this.padNumber(date.month) + '/' : '';
-            stringDate += this.isNumber(date.day) ? this.padNumber(date.day) + '/' : '';
+			stringDate += this.isNumber(date.month)
+				? this.padNumber(date.month) + "/"
+				: "";
+			stringDate += this.isNumber(date.day)
+				? this.padNumber(date.day) + "/"
+				: "";
 
 			stringDate += date.year;
-        }
-        return stringDate;
+		}
+		return stringDate;
 	}
 
 	/**
@@ -73,7 +77,7 @@ export class TypesUtilsService {
 	 */
 	getDateFormatterFromString(dateInStr: string): any {
 		if (dateInStr && dateInStr.length > 0) {
-			const dateParts = dateInStr.trim().split('/');
+			const dateParts = dateInStr.trim().split("/");
 			return [
 				{
 					year: this.toInteger(dateParts[2]),
@@ -98,9 +102,9 @@ export class TypesUtilsService {
 	 *
 	 * @param dateInStr: string (format => 'MM/dd/yyyy')
 	 */
-	getDateFromString(dateInStr: string = ''): Date {
+	getDateFromString(dateInStr: string = ""): Date {
 		if (dateInStr && dateInStr.length > 0) {
-			const dateParts = dateInStr.trim().split('/');
+			const dateParts = dateInStr.trim().split("/");
 			const year = this.toInteger(dateParts[2]);
 			const month = this.toInteger(dateParts[0]);
 			const day = this.toInteger(dateParts[1]);
@@ -115,7 +119,6 @@ export class TypesUtilsService {
 		return new Date();
 	}
 
-
 	/**
 	 * Convert Date to string with format 'MM/dd/yyyy'
 	 * @param _date: Date?
@@ -125,5 +128,39 @@ export class TypesUtilsService {
 		const year = _date.getFullYear();
 		const date = _date.getDate();
 		return `${month}/${date}/${year}`;
+	}
+
+	removeUnicode(str) {
+		str = str.toLowerCase();
+		//     We can also use this instead of from line 11 to line 17
+		//     str = str.replace(/\u00E0|\u00E1|\u1EA1|\u1EA3|\u00E3|\u00E2|\u1EA7|\u1EA5|\u1EAD|\u1EA9|\u1EAB|\u0103|\u1EB1|\u1EAF|\u1EB7|\u1EB3|\u1EB5/g, "a");
+		//     str = str.replace(/\u00E8|\u00E9|\u1EB9|\u1EBB|\u1EBD|\u00EA|\u1EC1|\u1EBF|\u1EC7|\u1EC3|\u1EC5/g, "e");
+		//     str = str.replace(/\u00EC|\u00ED|\u1ECB|\u1EC9|\u0129/g, "i");
+		//     str = str.replace(/\u00F2|\u00F3|\u1ECD|\u1ECF|\u00F5|\u00F4|\u1ED3|\u1ED1|\u1ED9|\u1ED5|\u1ED7|\u01A1|\u1EDD|\u1EDB|\u1EE3|\u1EDF|\u1EE1/g, "o");
+		//     str = str.replace(/\u00F9|\u00FA|\u1EE5|\u1EE7|\u0169|\u01B0|\u1EEB|\u1EE9|\u1EF1|\u1EED|\u1EEF/g, "u");
+		//     str = str.replace(/\u1EF3|\u00FD|\u1EF5|\u1EF7|\u1EF9/g, "y");
+		//     str = str.replace(/\u0111/g, "d");
+		str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+		str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+		str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+		str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+		str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+		str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+		str = str.replace(/đ/g, "d");
+		// Some system encode vietnamese combining accent as individual utf-8 characters
+		str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // Huyền sắc hỏi ngã nặng
+		str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // Â, Ê, Ă, Ơ, Ư
+		str = str.replace(/\s/g, "");
+		return str;
+	}
+	randomGuid() {
+		return "xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+			let r = (Math.random() * 16) | 0,
+				v = c == "x" ? r : (r & 0x3) | 0x8;
+			return v.toString(16);
+		});
+	}
+	isEmptyOrSpaces(str: string) {
+		return str === null || str.match(/^ *$/) !== null;
 	}
 }
