01e6742507be4c4fe378fbc17453fb462d0a49ff
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-22 00:37:36 +0700

    - Add class use for one line in dynamic form generate

--- src/app/views/pages/builder-manage/form-field-management/components/money/money.component.ts
+++ src/app/views/pages/builder-manage/form-field-management/components/money/money.component.ts
@@ -4,39 +4,40 @@ import { FieldConfigInterface } from "./../../../../../../core/auth";
 @Component({
 	selector: "app-money",
 	template: `
-		<mat-form-field [formGroup]="group">
-			<span matPrefix>{{ field.unitMoney.unit }} &nbsp;</span>
-			<input
-				type="text"
-                [(ngModel)]="previewInput"
-                #previewInputRef
-				ktNumbericDerective
-				numericType="number"
-				ktInputToggleComma
-				matInput
-				placeholder="{{field.name}}"
-				maxlength="24"
-				[formControlName]="field.name"
-				[placeholder]="field.name"
-				[type]="field.inputType"
-			/>
-			<mat-icon
-				matSuffix
-				*ngIf="field.description"
-				matTooltip="{{ field.description }}"
-				>local_atm</mat-icon
-			>
-            <ng-container
-                *ngFor="let validation of field.validations"
-                ngProjectAs="mat-error"
-            >
-                <mat-error
-                    *ngIf="group.get(field.name).hasError(validation.name)"
-                    >{{ validation.message }}</mat-error
-                >
-            </ng-container>
-            <mat-hint align="end">{{previewInputRef.value.length}} / 32</mat-hint>
-		</mat-form-field>
+		<div class="form-group kt-form__group">
+			<mat-form-field [formGroup]="group">
+				<span matPrefix>{{ field.unitMoney.unit }} &nbsp;</span>
+				<input
+					type="text"
+					[(ngModel)]="previewInput"
+					#previewInputRef
+					ktNumbericDerective
+					numericType="number"
+					ktInputToggleComma
+					matInput
+					placeholder="{{ field.name }}"
+					maxlength="24"
+					[formControlName]="field.name"
+					[placeholder]="field.name"
+					[type]="field.inputType"
+				/>
+				<mat-icon
+					matSuffix
+					*ngIf="field.description"
+					matTooltip="{{ field.description }}"
+					>local_atm</mat-icon
+				>
+				<ng-container
+					*ngFor="let validation of field.validations"
+					ngProjectAs="mat-error"
+				>
+					<mat-error
+						*ngIf="group.get(field.name).hasError(validation.name)"
+						>{{ validation.message }}</mat-error
+					>
+				</ng-container>
+			</mat-form-field>
+		</div>
 	`,
 	styles: []
 })
@@ -45,27 +46,6 @@ export class MoneyDynamicComponent implements OnInit {
 	group: FormGroup;
 	constructor() {}
 	ngOnInit() {
-		console.log(this.field.unitMoney);
+		// console.log(this.field.unitMoney);
 	}
 }
-
-// <mat-hint align="start" *ngIf="field.checked"
-// >{{ errorMessageRef?.value }}
-// </mat-hint>
-// <mat-form-field class="demo-full-width" [formGroup]="group">
-// <input
-//     matInput
-//     [formControlName]="field.name"
-//     [placeholder]="field.name"
-//     [type]="field.inputType"
-// />
-// <ng-container
-//     *ngFor="let validation of field.validations"
-//     ngProjectAs="mat-error"
-// >
-//     <mat-error
-//         *ngIf="group.get(field.name).hasError(validation.name)"
-//         >{{ validation.message }}</mat-error
-//     >
-// </ng-container>
-// </mat-form-field>
--- src/app/views/pages/builder-manage/form-field-management/components/select/select.component.ts
+++ src/app/views/pages/builder-manage/form-field-management/components/select/select.component.ts
@@ -4,125 +4,135 @@ import { FieldConfigInterface } from "./../../../../../../core/auth";
 @Component({
 	selector: "app-select",
 	template: `
-		<ng-container [ngSwitch]="field.fieldType">
-			<ng-container *ngSwitchCase="'Option Set'">
-				<mat-form-field [formGroup]="group" class="kt-margin-r-20">
-					<mat-label>{{ field.label }}</mat-label>
-					<mat-select
-						[placeholder]="field.label"
-						[formControlName]="field.name"
-					>
-						<mat-option
-							*ngFor="let item of field.options"
-							[value]="item.value"
-							>{{ item.label }}
-						</mat-option>
-					</mat-select>
-					<mat-icon
-						*ngIf="field.description"
-						color="primary"
-						matSuffix
-						matTooltip="{{ field.description }}"
-						>help</mat-icon
-					>
-					<ng-container
-						*ngFor="let validation of field.validations"
-						ngProjectAs="mat-error"
-					>
-						<mat-error
-							*ngIf="
-								group.get(field.name).hasError(validation.name)
-							"
-							>{{ validation.message }}</mat-error
+		<div class="form-group kt-form__group">
+			<ng-container [ngSwitch]="field.fieldType">
+				<ng-container *ngSwitchCase="'Option Set'">
+					<mat-form-field [formGroup]="group" class="kt-margin-r-20">
+						<mat-label>{{ field.label }}</mat-label>
+						<mat-select
+							[placeholder]="field.label"
+							[formControlName]="field.name"
 						>
-					</ng-container>
-				</mat-form-field>
-			</ng-container>
-
-			<ng-container *ngSwitchCase="'MultiSelect Option Set'">
-				<mat-form-field [formGroup]="group" class="kt-margin-r-20">
-					<mat-select
-						[placeholder]="field.label"
-						[formControlName]="field.name"
-						multiple
-					>
-						<mat-select-trigger>
-							{{ field.name.value ? field.name.value[0] : "" }}
-							<span
-								*ngIf="field.name.value?.length > 1"
-								class="example-additional-selection"
+							<mat-option
+								*ngFor="let item of field.options"
+								[value]="item.value"
+								>{{ item.label }}
+							</mat-option>
+						</mat-select>
+						<mat-icon
+							*ngIf="field.description"
+							color="primary"
+							matSuffix
+							matTooltip="{{ field.description }}"
+							>help</mat-icon
+						>
+						<ng-container
+							*ngFor="let validation of field.validations"
+							ngProjectAs="mat-error"
+						>
+							<mat-error
+								*ngIf="
+									group
+										.get(field.name)
+										.hasError(validation.name)
+								"
+								>{{ validation.message }}</mat-error
 							>
-								(+{{ field.name.value.length - 1 }}
-								{{
-									field.name.value?.length === 2
-										? "other"
-										: "others"
-								}})
-							</span>
-						</mat-select-trigger>
-						<mat-option
-							*ngFor="let item of field.options"
-							[value]="item.value"
-							>{{ item.label }}
-						</mat-option>
-					</mat-select>
-					<mat-icon
-						*ngIf="field.description"
-						color="primary"
-						matSuffix
-						matTooltip="{{ field.description }}"
-						>help</mat-icon
-					>
-					<ng-container
-						*ngFor="let validation of field.validations"
-						ngProjectAs="mat-error"
-					>
-						<mat-error
-							*ngIf="
-								group.get(field.name).hasError(validation.name)
-							"
-							>{{ validation.message }}</mat-error
-						>
-					</ng-container>
-				</mat-form-field>
-			</ng-container>
+						</ng-container>
+					</mat-form-field>
+				</ng-container>
 
-			<ng-container *ngSwitchCase="'Two Option'">
-				<mat-form-field [formGroup]="group" class="kt-margin-r-20">
-					<mat-label>{{ field.label }}</mat-label>
-					<mat-select
-						[placeholder]="field.label"
-						[formControlName]="field.name"
-						multiple
-						(selectionChange)="handleChangeTwoOptions()"
-					>
-						<mat-option
-							*ngFor="let item of field.options"
-							[value]="item.value"
-							>{{ item.label }}
-						</mat-option>
-					</mat-select>
-					<mat-icon
-						*ngIf="field.description"
-						color="primary"
-						matSuffix
-						matTooltip="{{ field.description }}"
-						>help</mat-icon
-					>
-					<ng-container
-						*ngFor="let validation of field.validations"
-						ngProjectAs="mat-error"
-					>
-						<mat-error
-							*ngIf="
-								group.get(field.name).hasError(validation.name)
-							"
-							>{{ validation.message }}</mat-error
+				<ng-container *ngSwitchCase="'MultiSelect Option Set'">
+					<mat-form-field [formGroup]="group" class="kt-margin-r-20">
+						<mat-select
+							[placeholder]="field.label"
+							[formControlName]="field.name"
+							multiple
 						>
-					</ng-container>
-				</mat-form-field>
+							<mat-select-trigger>
+								{{
+									field.name.value ? field.name.value[0] : ""
+								}}
+								<span
+									*ngIf="field.name.value?.length > 1"
+									class="example-additional-selection"
+								>
+									(+{{ field.name.value.length - 1 }}
+									{{
+										field.name.value?.length === 2
+											? "other"
+											: "others"
+									}})
+								</span>
+							</mat-select-trigger>
+							<mat-option
+								*ngFor="let item of field.options"
+								[value]="item.value"
+								>{{ item.label }}
+							</mat-option>
+						</mat-select>
+						<mat-icon
+							*ngIf="field.description"
+							color="primary"
+							matSuffix
+							matTooltip="{{ field.description }}"
+							>help</mat-icon
+						>
+						<ng-container
+							*ngFor="let validation of field.validations"
+							ngProjectAs="mat-error"
+						>
+							<mat-error
+								*ngIf="
+									group
+										.get(field.name)
+										.hasError(validation.name)
+								"
+								>{{ validation.message }}</mat-error
+							>
+						</ng-container>
+					</mat-form-field>
+				</ng-container>
+
+				<ng-container *ngSwitchCase="'Two Option'">
+					<mat-form-field [formGroup]="group" class="kt-margin-r-20">
+						<mat-label>{{ field.label }}</mat-label>
+						<mat-select
+							[placeholder]="field.label"
+							[formControlName]="field.name"
+							multiple
+							(selectionChange)="handleChangeTwoOptions()"
+						>
+							<mat-option
+								*ngFor="let item of field.options"
+								[value]="item.value"
+								>{{ item.label }}
+							</mat-option>
+						</mat-select>
+						<mat-icon
+							*ngIf="field.description"
+							color="primary"
+							matSuffix
+							matTooltip="{{ field.description }}"
+							>help</mat-icon
+						>
+						<ng-container
+							*ngFor="let validation of field.validations"
+							ngProjectAs="mat-error"
+						>
+							<mat-error
+								*ngIf="
+									group
+										.get(field.name)
+										.hasError(validation.name)
+								"
+								>{{ validation.message }}</mat-error
+							>
+						</ng-container>
+					</mat-form-field>
+				</ng-container>
 			</ng-container>
-		</ng-container>
+		</div>
 	`,
 	styles: []
 })
