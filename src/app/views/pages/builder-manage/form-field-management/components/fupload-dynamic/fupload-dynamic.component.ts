1b5254aa617b5e32848498883b40887e8489fc84
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-22 00:32:54 +0700

    - Add fuploadDynamicComponent to form dynamic

---
+++ src/app/views/pages/builder-manage/form-field-management/components/fupload-dynamic/fupload-dynamic.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { FieldConfigInterface } from "./../../../../../../core/auth";
+@Component({
+	selector: "app-fupload",
+	template: `
+		<div class="form-group kt-form__group" [formGroup]="group">
+			<app-material-file-upload
+				(complete)="onFileComplete($event)"
+				[RequireMessage]="require.message"
+				[multiple]="field.fieldType"
+				[Text]="field.textFupload"
+				[controlName]="formControl"
+				[controlGroup]="group"
+			></app-material-file-upload>
+		</div>
+	`,
+	styles: []
+})
+export class FuploadDynamicComponent implements OnInit {
+	field: FieldConfigInterface;
+	group: FormGroup;
+	formControl: any;
+	require: any;
+	constructor() {}
+	ngOnInit() {
+		// validate require
+		this.require = this.field.validations[0];
+		this.formControl = new FormControl(this.field.name);
+	}
+	onFileComplete(data){
+		console.log(data);
+		this.group.controls[this.field.name].setValue(data);
+	}
+}
