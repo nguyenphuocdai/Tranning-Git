ca3b1be63de42d258f5a66e33b4a0979b4425609
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-25 21:02:48 +0700

    - Add function generate Guid for data

--- src/app/core/_base/crud/utils/types-utils.service.ts
+++ src/app/core/_base/crud/utils/types-utils.service.ts
@@ -186,4 +186,16 @@ export class TypesUtilsService {
 		max = Math.floor(max);
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
+	makeid(length: number = 10) {
+		let result = "";
+		let characters =
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		let charactersLength = characters.length;
+		for (let i = 0; i < length; i++) {
+			result += characters.charAt(
+				Math.floor(Math.random() * charactersLength)
+			);
+		}
+		return result;
+	}
 }
