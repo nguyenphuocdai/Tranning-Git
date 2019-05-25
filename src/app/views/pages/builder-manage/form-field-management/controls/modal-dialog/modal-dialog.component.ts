feefe8b964c63a9ecc0abe1417f0093ac91304df
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-25 21:05:38 +0700

    - Receive data from component (valueEdit)

--- src/app/views/pages/builder-manage/form-field-management/controls/modal-dialog/modal-dialog.component.ts
+++ src/app/views/pages/builder-manage/form-field-management/controls/modal-dialog/modal-dialog.component.ts
@@ -1,3 +1,4 @@
+import { FieldConfigInterface } from "./../../../../../../shared/_model-app/field.interface";
 import { DialogRefInterface } from "../../../../../../shared/_model-app/dialog-ref.interface";
 import { Component, OnInit, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
@@ -9,19 +10,23 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 })
 export class ModalDialogComponent implements OnInit {
 	dialogRefData: DialogRefInterface;
+	valueEdit: FieldConfigInterface;
 	constructor(
 		@Inject(MAT_DIALOG_DATA) public data: DialogRefInterface,
 		private dialogRef: MatDialogRef<ModalDialogComponent>
 	) {
 		this.dialogRefData = data;
+		if (data.valueEdit) {
+			this.valueEdit = data.valueEdit;
+		}
 	}
 
 	ngOnInit() {}
 	/**
 	 * receive object data from component child
-	 * @param event
+	 * @param data
 	 */
-	onSubmit(event) {
-		this.dialogRef.close(event);
+	onSubmit(data) {
+		this.dialogRef.close(data);
 	}
 }
