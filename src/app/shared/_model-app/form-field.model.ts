5329ba6caa744815b7335541a22f8dac316dc6ca
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-25 17:53:18 +0700

    - Temporary hidden fieldmodel

--- src/app/shared/_model-app/form-field.model.ts
+++ src/app/shared/_model-app/form-field.model.ts
@@ -1,10 +1,10 @@
-import { BaseBuilderModel } from "../../core/_base/crud/models/_base-builder.model";
+// import { BaseBuilderModel } from "../../core/_base/crud/models/_base-builder.model";
 
-export class ModuleModel extends BaseBuilderModel {
-	roles: number[];
-	image: string;
+// export class FieldModel extends BaseBuilderModel {
+// 	roles: number[];
+// 	image: string;
 
-	clear(): void {
-		// clear solution
-	}
-}
+// 	clear(): void {
+// 		// clear solution
+// 	}
+// }
