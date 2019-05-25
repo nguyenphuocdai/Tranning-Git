c0b10e117a93d443b52ff2d08364293fbd84d725
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-25 21:03:54 +0700

    - Add valueEdit to interface dialog, cause pass data edit to modal

--- src/app/shared/_model-app/dialog-ref.interface.ts
+++ src/app/shared/_model-app/dialog-ref.interface.ts
@@ -1,5 +1,8 @@
+import { FieldConfigInterface } from "./field.interface";
+
 export interface DialogRefInterface {
 	type: string;
 	valueView?: string;
 	inputType?: string;
+	valueEdit?: FieldConfigInterface;
 }
