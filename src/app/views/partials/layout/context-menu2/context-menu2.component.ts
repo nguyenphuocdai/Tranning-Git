0f63021a6b716cad944c59c377f81bf5915031d4
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-27 12:23:42 +0700

    - Change kt-context-menu2 -> kt-context-menu02

--- src/app/views/partials/layout/context-menu2/context-menu2.component.ts
+++ src/app/views/partials/layout/context-menu2/context-menu2.component.ts
@@ -1,13 +1,12 @@
 // Angular
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 /**
  * Sample context menu dropdown
  */
 @Component({
-	selector: 'kt-context-menu2',
-	templateUrl: './context-menu2.component.html',
-	styleUrls: ['./context-menu2.component.scss']
+	selector: "kt-context-menu02",
+	templateUrl: "./context-menu2.component.html",
+	styleUrls: ["./context-menu2.component.scss"]
 })
-export class ContextMenu2Component {
-}
+export class ContextMenu2Component {}
