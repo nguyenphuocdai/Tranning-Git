f81b347cc34af0ee1511b365875a1b10c0737b9f
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-27 12:23:13 +0700

    - Page TEST export-as

--- src/app/views/pages/my-page/my-page.component.ts
+++ src/app/views/pages/my-page/my-page.component.ts
@@ -1,3 +1,6 @@
+import { ExportJsonToExcelService } from './../../../shared/lib/export-json-to-excel/export-json-to-excel.service';
+import { ExportAsConfig } from './../../../shared/lib/ngx-export-as/export-as-config.model';
+import { ExportAsService } from './../../../shared/lib/ngx-export-as/export-as.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-page.component.scss']
 })
 export class MyPageComponent implements OnInit {
-
-  constructor() { }
+  config: ExportAsConfig = {
+    type: 'pdf',
+    elementId: 'mytable',
+  };
+  persons: Person[];
+  constructor( private exportAsService: ExportAsService, private exportJsonToExcelService: ExportJsonToExcelService) {
+    this.exportJsonToExcelService = exportJsonToExcelService;
+    this.persons = PERSONS;
+   }
 
   ngOnInit() {
   }
-
+  // exportAs(type) {
+  //   this.config.type = type;
+  //   this.exportAsService.save(this.config, 'myFile');
+  //   // this.exportAsService.get(this.config).subscribe(content => {
+  //   //   console.log(content);
+  //   // });
+  // }
+  exportToExcel(event) {
+    this.exportJsonToExcelService.exportAsExcelFile(this.persons, 'persons');
+  }
 }
+
+
+export class Person {
+  id: number;
+  name: String;
+  surname: String;
+  age: number;
+}
+
+
+export const PERSONS: Person[] = [
+  {
+      id: 1,
+      name: 'Very long name which needs to be wrapped',
+      surname: 'Novicky',
+      age: 21
+  },
+  {
+      id: 2,
+      name: 'Another long name that won\'t be wrapped',
+      surname: 'Tracz',
+      age: 12
+  },
+  {
+      id: 3,
+      name: 'Steve',
+      surname: 'Laski',
+      age: 38
+  }
+];
