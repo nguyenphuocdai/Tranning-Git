6588b0647dd8ad4f70de6f61315427344ec48b31
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-30 23:13:36 +0700

    - Add module management

---
+++ src/app/views/pages/apps-management/apps-management-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppsManagementComponent } from "./apps-management.component";
+import { ManagementComponent } from "./management/management.component";
+
+// tslint:disable-next-line:class-name
+const routes: Routes = [
+	{
+		path: "",
+		component: AppsManagementComponent,
+		children: [
+			{
+				path: "",
+				redirectTo: "management",
+				pathMatch: "full"
+			},
+			{
+				path: "management",
+				component: ManagementComponent
+			}
+		]
+	}
+];
+
+@NgModule({
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
+})
+export class AppsManagementRoutingModule {}
---
+++ src/app/views/pages/apps-management/apps-management.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kt-apps-management',
+  templateUrl: './apps-management.component.html'
+})
+export class AppsManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
