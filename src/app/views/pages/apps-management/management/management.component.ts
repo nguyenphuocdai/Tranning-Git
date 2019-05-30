52d61c5e0e139fa3efd205e7a5386c2b6e7929c2
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-05-30 23:13:17 +0700

    - Add management solution

---
+++ src/app/views/pages/apps-management/management/management.component.ts
@@ -0,0 +1,38 @@
+import { SolutionModel } from "./../../../../shared/_model-app/solution.model";
+import { LocalstorageService } from "./../../../../shared/_services/local-storage-service/localstorage.service";
+import { AppSettings } from "./../../../../shared/_constant/app-setting";
+import {
+	Component,
+	OnInit,
+	ViewChild,
+	ChangeDetectionStrategy
+} from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatTableDataSource } from "@angular/material/table";
+
+/**
+ * @title Table with pagination
+ */
+@Component({
+	selector: "kt-solution",
+	templateUrl: "./management.component.html",
+	styleUrls: ["./management.component.scss"],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ManagementComponent implements OnInit {
+	displayedColumns: string[] = ["id", "name", "owner", "version"];
+	dataSource: MatTableDataSource<SolutionModel>;
+	listSolution: SolutionModel[] = [];
+	constructor(private localstorageService: LocalstorageService) {}
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+
+	ngOnInit() {
+		let listSolution: SolutionModel[] = this.localstorageService.get(
+			AppSettings.SOLUTIONSTORAGE
+		);
+		this.listSolution = listSolution;
+
+		this.dataSource = new MatTableDataSource(this.listSolution);
+		this.dataSource.paginator = this.paginator;
+	}
+}
