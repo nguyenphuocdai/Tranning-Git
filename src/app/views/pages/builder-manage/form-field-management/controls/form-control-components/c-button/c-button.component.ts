466734b2a5348a7c0ecab03fc0d4d3c66eb54218
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-06-01 21:02:40 +0700

    - Add property database to c-button

--- src/app/views/pages/builder-manage/form-field-management/controls/form-control-components/c-button/c-button.component.ts
+++ src/app/views/pages/builder-manage/form-field-management/controls/form-control-components/c-button/c-button.component.ts
@@ -31,7 +31,7 @@ export class CButtonComponent implements OnInit {
 	@Input("valueEdit") valueEdit: FieldConfigInterface;
 	@Output("cButtonComponentSubmit") submitForm = new EventEmitter<object>();
 
-	rfField: FormGroup;
+	rfButton: FormGroup;
 	optionDefault: string = "button";
 	isSubmit: boolean = false;
 	constructor(
@@ -44,15 +44,15 @@ export class CButtonComponent implements OnInit {
 	ngOnInit() {
 		this.createForm();
 		if (this.valueEdit) {
-			this.rfField.controls["name"].setValue(this.valueEdit.name);
-			this.rfField.controls["required"].setValue(this.valueEdit.required);
-			this.rfField.controls["errorMessage"].setValue(
+			this.rfButton.controls["name"].setValue(this.valueEdit.name);
+			this.rfButton.controls["required"].setValue(this.valueEdit.required);
+			this.rfButton.controls["errorMessage"].setValue(
 				this.valueEdit.errorMessage
 			);
-			this.rfField.controls["security"].setValue(this.valueEdit.security);
-			this.rfField.controls["tracking"].setValue(this.valueEdit.tracking);
-			this.rfField.controls["fieldType"].setValue(
-				this.valueEdit.fieldType
+			this.rfButton.controls["security"].setValue(this.valueEdit.security);
+			this.rfButton.controls["tracking"].setValue(this.valueEdit.tracking);
+			this.rfButton.controls["database"].setValue(
+				this.valueEdit.database
 			);
 		}
 	}
@@ -61,13 +61,13 @@ export class CButtonComponent implements OnInit {
 	 * create form builder
 	 */
 	createForm() {
-		this.rfField = this._fbField.group({
+		this.rfButton = this._fbField.group({
 			name: ["", Validators.required],
 			required: new FormControl(false),
 			errorMessage: [""],
 			security: new FormControl(false),
 			tracking: new FormControl(false),
-			fieldType: ["", Validators.required]
+			database: ["", Validators.required]
 		});
 	}
 
@@ -77,7 +77,7 @@ export class CButtonComponent implements OnInit {
 	onSubmit(event) {
 		this.isSubmit = true;
 		this._dialogRef.disableClose = true;
-		if (this.rfField.invalid) {
+		if (this.rfButton.invalid) {
 			this.isSubmit = false;
 			return;
 		}
@@ -97,14 +97,14 @@ export class CButtonComponent implements OnInit {
 	 * build data fro @Output
 	 */
 	onBuildData() {
-		let label = this.rfField.controls["name"].value;
+		let label = this.rfButton.controls["name"].value;
 		let type = this.dialogRefData.type;
 		let inputType = this.dialogRefData.valueView;
-		let isRequired = this.rfField.controls["required"].value;
-		let errorMessage = this.rfField.controls["errorMessage"].value;
-		let isSecurity = this.rfField.controls["security"].value;
-		let isTracking = this.rfField.controls["tracking"].value;
-		let fieldType = this.rfField.controls["fieldType"].value;
+		let isRequired = this.rfButton.controls["required"].value;
+		let errorMessage = this.rfButton.controls["errorMessage"].value;
+		let isSecurity = this.rfButton.controls["security"].value;
+		let isTracking = this.rfButton.controls["tracking"].value;
+		let database = this.rfButton.controls["database"].value;
 
 		let mergedObj: FieldConfigInterface = {
 			id:
@@ -117,7 +117,7 @@ export class CButtonComponent implements OnInit {
 			name: label,
 			security: isSecurity,
 			tracking: isTracking,
-			fieldType: fieldType,
+			database: database,
 			validations: []
 		};
 		if (isRequired === true) {
@@ -128,14 +128,7 @@ export class CButtonComponent implements OnInit {
 			};
 			mergedObj.validations.push(objValidator);
 		}
-		// if (isPattern) {
-		// 	let objValidator = {
-		// 		name: "pattern",
-		// 		validator: Validators.pattern(isPattern),
-		// 		message: messagePattern
-		// 	};
-		// 	mergedObj.validations.push(objValidator);
-		// }
+		
 		return mergedObj;
 	}
 }
