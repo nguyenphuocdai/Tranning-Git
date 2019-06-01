84419392aae9ec97d0288f0962aea5d220801db2
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-06-01 21:05:15 +0700

    - Temporary format number decimal has comma & dot

--- src/app/views/pages/builder-manage/form-field-management/components/number/number.component.ts
+++ src/app/views/pages/builder-manage/form-field-management/components/number/number.component.ts
@@ -16,87 +16,87 @@ import { TypesUtilsService } from "../../../../../../core/_base/crud";
 export class NumberComponent implements OnInit {
 	field: FieldConfigInterface;
 	group: FormGroup;
-	// isShowMinMaxError: boolean;
-	constructor(private _typeUltiService: TypesUtilsService) {}
+	hasError: boolean = false;
+	constructor() {}
 	ngOnInit() {}
-	// onCheckMinMax(min, max) {
-	// 	this.isShowMinMaxError = true;
-	// 	let previewInput = this.group.controls[this.field.name].value;
-
-	// 	if (
-	// 		min === undefined ||
-	// 		max === undefined ||
-	// 		previewInput === undefined
-	// 	) {
-	// 		return;
-	// 	}
-	// 	let fMinValue = parseFloat(min.replace(",", ""));
-	// 	let fMaxValue = parseFloat(max.replace(",", ""));
-	// 	let floatInput = parseFloat(previewInput.replace(",", ""));
-	// 	if (floatInput >= fMinValue && floatInput <= fMaxValue) {
-	// 		this.isShowMinMaxError = false;
-	// 	}
-	// }
-	// onFocus() {
-	// 	let value = this.group.controls[this.field.name].value;
-	// 	if (value === undefined || value === null || value.length <= 3) {
-	// 		return;
-	// 	}
-	// 	this.group.controls[this.field.name].setValue(
-	// 		this._typeUltiService.digitRemoveComma(value)
-	// 	);
-	// }
-	// onBlur() {
-	// 	let value = this.group.controls[this.field.name].value;
-	// 	if (value === undefined || value === null || value.length <= 3) {
-	// 		return;
-	// 	}
-	// 	this.group.controls[this.field.name].setValue(
-	// 		this._typeUltiService.digitAddComma(value)
-	// 	);
-	// }
 	onNumberChange(event) {
-		console.log(event);
-		let regexpNumber = new RegExp(/^\d+$/);
-		// let allowKey =  [
-		// 	"Backspace",
-		// 	"Tab",
-		// 	"End",
-		// 	"Home",
-		// 	"ArrowLeft",
-		// 	"ArrowRight",
-		// 	","
-		// ];
-		// if (allowKey.indexOf(event.key) !== -1) {
+		// let regexpNumber = new RegExp(/^(\d+$)([\.,])+$/);
+
+		let controlValue = this.group.controls[this.field.name].value;
+		if (!controlValue) {
+			return;
+		}
+
+		// if (controlValue.match(regexpNumber)) {
+		// 	console.log(1);
+		// 	return;
+		// } else {
+		// 	this.group.controls[this.field.name].setValue(controlValue.replace(event.key,""));
+		// 	// console.log(2);
 		// 	return;
 		// }
-		// const charCode = (event.which) ? event.which : event.keyCode;
-		// if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-		//   return false;
+		// if (!this.group.controls[this.field.name].value) {
+		// 	return;
 		// }
-		// Do not use event.keycode this is deprecated.
-		let current: string = this.group.controls[this.field.name].value.replace(/,/g, "");
-		let next: string = current.concat(event.key);
-		if (
-			String(next.replace(/,/g, "")).match(regexpNumber) &&
-			next.replace(/,/g, "").length > 3
-		) {
-			let valueRemoveComma = next.replace(/,/g, "");
-			let valueAddcomma = valueRemoveComma.replace(
-				/\B(?=(\d{3})+(?!\d))/g,
-				","
-			);
-			setTimeout(() => {
-				// let control = this.group.controls[this.field.name];
-				// let controlValue = this.group.controls[this.field.name].value;
-				// let numberDecimal = parseInt(this.group.controls[this.field.numberDecimal].value,10);
+		// let current: string = this.group.controls[
+		// 	this.field.name
+		// ].value.replace(/,/g, "");
+		// let next: string = current.concat(event.key);
+		// console.log(String(next.replace(/,/g, "")).match(regexpNumber));
+		// if (
+		// 	String(next.replace(/,/g, "")).match(regexpNumber) &&
+		// 	next.replace(/,/g, "").length > 3
+		// ) {
+		// 	let valueRemoveComma = next.replace(/,/g, "");
+		// 	let valueAddcomma = valueRemoveComma.replace(
+		// 		/\B(?=(\d{3})+(?!\d))/g,
+		// 		","
+		// 	);
 
-				// let arrSplitpreviewInput = controlValue.split(".");
-				// control.setValue(arrSplitpreviewInput[0]);
-				// let valuePadEnd = controlValue.concat(".").padEnd(controlValue.length + 1 + numberDecimal, "0");
+		// 	if (valueAddcomma.split(".").length === 2) {
+		// 		let numberDecimal = parseInt(
+		// 			this.group.controls[this.field.name].value,
+		// 			10
+		// 		);
+		// 		this.maxlength = valueAddcomma.length + 1 + numberDecimal;
+		// 	}
 
-				this.group.controls[this.field.name].setValue(valueAddcomma);
-			}, 10);
+		// 	setTimeout(() => {
+		// 		this.group.controls[this.field.name].setValue(valueAddcomma);
+		// 	}, 10);
+		// }
+		// }
+	}
+	onBlur() {
+		this.hasError = false;
+		let numberDecimal = parseInt(this.field.numberDecimal, 10);
+		let regexDecimal = new RegExp(
+			/^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:(\.|,)\d{1,12})?$/
+		);
+		let controlValue = this.group.controls[this.field.name].value;
+		if (!controlValue) {
+			return;
+		}
+
+		if (controlValue.match(regexDecimal)) {
+			console.log(1);
+			let arrSplit = controlValue.split(".");
+			let valueAddcomma;
+			if (arrSplit.length === 2) {
+				valueAddcomma = arrSplit[0].replace(
+					/\B(?=(\d{3})+(?!\d))/g,
+					","
+				);
+				valueAddcomma = valueAddcomma.concat("." + arrSplit[1]);
+			} else {
+				valueAddcomma = controlValue.replace(
+					/\B(?=(\d{3})+(?!\d))/g,
+					","
+				);
+			}
+			this.group.controls[this.field.name].setValue(valueAddcomma);
+		} else {
+			this.hasError = true;
 		}
 	}
 }
