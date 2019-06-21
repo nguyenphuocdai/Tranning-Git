f5ef829dccefea20810b3dd2a4cf4b8f82918b7e
dainguyenAlttekglobal <dai.nguyen@alttekglobal.com>
2019-06-21 23:54:56 +0700

    - Update binding array files image

--- src/app/shared/components/preview-image/preview-image.component.ts
+++ src/app/shared/components/preview-image/preview-image.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA } from "@angular/material";
+import { Lightbox } from "ngx-lightbox";
 
 @Component({
 	selector: "kt-preview-image",
@@ -7,10 +8,41 @@ import { MAT_DIALOG_DATA } from "@angular/material";
 	styleUrls: ["./preview-image.component.scss"]
 })
 export class PreviewImageComponent implements OnInit {
-	constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 	file: any;
+	_album = [];
+
+	constructor(
+		@Inject(MAT_DIALOG_DATA) public data: any,
+		private _lightbox: Lightbox
+	) {
+		for (let i = 0; i < this.data.file.length; i++) {
+			// const src =
+			// 	"../../../../assets/media/products/product" + i + ".jpg";
+			// const caption = "Image " + i + " caption here";
+			// const thumb =
+			// 	"../../../../assets/media/products/product" + i + ".jpg";
+			const element = this.data.file[i];
+			const album = {
+				src: element.base64,
+				thumb: element.base64
+			};
+
+			this._album.push(album);
+		}
+	}
+
 	ngOnInit() {
 		this.file = this.data.file;
-		console.log(this.data);
+		console.log(this.data.file);
+	}
+
+	open(index: number): void {
+		// open lightbox
+		this._lightbox.open(this._album, index);
+	}
+
+	close(): void {
+		// close lightbox programmatically
+		this._lightbox.close();
 	}
 }
